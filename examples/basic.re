16bee1bcb91fafd8440fc49a27f30240891ec3d9
Sawyer Bergeron <sawyerbergeron@gmail.com>
2019-08-18 12:50:03 -0400

    WIP: sharing to see why compilation fails

--- examples/basic.re
+++ examples/basic.re
@@ -1,27 +1,33 @@
 external hello_sdl: unit => unit = "hello_sdl";
-external resdl_init: (~audio: bool=false,
-                      ~video: bool=true,
-                      ~events: bool=true,
-                      ~haptic: bool=false,
-                      ~timer: bool=false,
-                      ~gamecontroller: bool=false,
-                      ~joystick: bool=false,
-                      ~everything: bool=false)
-        : int => "resdl_init";
 
-external resdl_quit: (~audio: bool=false,
-                      ~video: bool=true,
-                      ~events: bool=true,
-                      ~haptic: bool=false,
-                      ~timer: bool=false,
-                      ~gamecontroller: bool=false,
-                      ~joystick: bool=false,
-                      ~everything: bool=false)
-        : int => "resdl_quit";
+external resdl_internal_init: (
+                  ~audio: bool,
+                  ~video: bool,
+                  ~events: bool,
+                  ~haptic: bool,
+                  ~timer: bool,
+                  ~gamecontroller: bool,
+                  ~joystick: bool,
+                  ~everything: bool) => int = "resdl_init";
+
+let resdl_init = (~audio: bool=false,
+                  ~video: bool=true,
+                  ~events: bool=true,
+                  ~haptic: bool=false,
+                  ~timer: bool=false,
+                  ~gamecontroller: bool=false,
+                  ~joystick: bool=false,
+                  ~everything: bool=false) : int => {
+
+    let ret:int = resdl_internal_init(audio, video, events, haptic, timer, gamecontroller, joystick, everything);
+
+    ret
+}
+
+external resdl_quit: unit => unit = "resdl_quit";
 
 let run = () => {
   /*let _ = hello_sdl();*/
   let _ = resdl_init(~audio=false, ~video=true, ~events=true);
   let _ = resdl_quit(~audio=false, ~video=true, ~events=true);
-  //let window = resdl_CreateWindow(
 };
--- src/glfw_wrapper.cpp
+++ src/glfw_wrapper.cpp
@@ -18,99 +18,6 @@
 #include <SDL2/SDL.h>
 
 extern "C" {
-    CAMLprim value hello_sdl() {
-        printf("hello, world\n");
-
-        SDL_Window *window;
-        int init_ret = SDL_Init(SDL_INIT_VIDEO);
-        printf("SDL_Init returned %d\n", init_ret);
-            // Create an application window with the following settings:
-
-    window = SDL_CreateWindow(
-        "An SDL2 window",                  // window title
-        20,           // initial x position
-        20,           // initial y position
-        640,                               // width, in pixels
-        480,                               // height, in pixels
-        SDL_WINDOW_OPENGL                  // flags - see below
-    );
-
-    printf("SDL_CreateWindow returned %p\n", window);
-    // Check that the window was successfully created
-    if (window == NULL) {
-        // In the case that the window could not be made...
-        printf("Could not create window: %s\n", SDL_GetError());
-        return 1;
-    }
-    // The window is open: could enter program loop here (see SDL_PollEvent())
-    for( int i = 0; i < 50; SDL_Delay(2000), i++ )
-    {
-        SDL_Surface* window_surface = SDL_GetWindowSurface(window);
-        SDL_FillRect(window_surface, NULL, SDL_MapRGB(window_surface->format, (i*1234) % 255, (i*1256) % 255, (i*1356) % 255));
-        SDL_UpdateWindowSurface(window);
-    }
-
-    // Close and destroy the window
-    SDL_DestroyWindow(window);
-    
-    // Clean up
-    SDL_Quit();
-
-        return Val_unit;
-    }
-
-    CAMLprim value resdl_init(
-            value v_audio,
-            value v_video,
-            value v_events,
-            value v_haptic,
-            value v_timer,
-            value v_gamecontroller,
-            value v_joystick,
-            value v_everything
-        )
-    {
-        printf("doing sdl setup\n");
-
-        uint32_t flags = priv_bitfield_from_flags( 
-            value v_audio,
-            value v_video,
-            value v_events,
-            value v_haptic,
-            value v_timer,
-            value v_gamecontroller,
-            value v_joystick,
-            value v_everything
-        );
-
-        SDL_InitSubSystem(flags);
-    }
-
-    CAMLprim value resdl_quit(
-            value v_audio,
-            value v_video,
-            value v_events,
-            value v_haptic,
-            value v_timer,
-            value v_gamecontroller,
-            value v_joystick,
-            value v_everything
-        )
-    {
-        uint32_t flags = priv_bitfield_from_flags( 
-            value v_audio,
-            value v_video,
-            value v_events,
-            value v_haptic,
-            value v_timer,
-            value v_gamecontroller,
-            value v_joystick,
-            value v_everything
-        );
-
-        SDL_QuitSubSystem(flags);
-    }
-
     static inline uint32_t priv_bitfield_from_flags(
             value v_audio,
             value v_video,
@@ -134,6 +41,83 @@ extern "C" {
         uint32_t i_everything = (i_everything) ? SDL_INIT_EVERYTHING : 0;
 
         return i_audio | i_video | i_events | i_haptic | i_timer | i_gamecontroller | i_joystick | i_everything;
+    }
+
+    CAMLprim value hello_sdl() {
+        printf("hello, world\n");
+
+        SDL_Window *window;
+        int init_ret = SDL_Init(SDL_INIT_VIDEO);
+        printf("SDL_Init returned %d\n", init_ret);
+            // Create an application window with the following settings:
+
+        window = SDL_CreateWindow(
+            "An SDL2 window",                  // window title
+            20,           // initial x position
+            20,           // initial y position
+            640,                               // width, in pixels
+            480,                               // height, in pixels
+            SDL_WINDOW_OPENGL                  // flags - see below
+        );
+
+        printf("SDL_CreateWindow returned %p\n", window);
+        // Check that the window was successfully created
+        if (window == NULL) {
+            // In the case that the window could not be made...
+            printf("Could not create window: %s\n", SDL_GetError());
+            return 1;
+        }
+        // The window is open: could enter program loop here (see SDL_PollEvent())
+        for( int i = 0; i < 50; SDL_Delay(2000), i++ )
+        {
+            SDL_Surface* window_surface = SDL_GetWindowSurface(window);
+            SDL_FillRect(window_surface, NULL, SDL_MapRGB(window_surface->format, (i*1234) % 255, (i*1256) % 255, (i*1356) % 255));
+            SDL_UpdateWindowSurface(window);
+        }
+
+        // Close and destroy the window
+        SDL_DestroyWindow(window);
+        
+        // Clean up
+        SDL_Quit();
+
+        return Val_unit;
+    }
+
+    CAMLprim value resdl_init(
+            value v_audio,
+            value v_video,
+            value v_events,
+            value v_haptic,
+            value v_timer,
+            value v_gamecontroller,
+            value v_joystick,
+            value v_everything
+        )
+    {
+        printf("doing sdl setup\n");
+
+        uint32_t flags = priv_bitfield_from_flags( 
+            v_audio,
+            v_video,
+            v_events,
+            v_haptic,
+            v_timer,
+            v_gamecontroller,
+            v_joystick,
+            v_everything
+        );
+
+        // 0 on success, <0 on failure
+        int e_code = SDL_InitSubSystem(flags);
+
+        return Val_int(e_code);
+    }
+
+    CAMLprim value resdl_quit() {
+        SDL_Quit();
+
+        return Val_unit;
     }
 }
 

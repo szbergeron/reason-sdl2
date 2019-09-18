e6f98513f40ee1295540282ba5005dc1ad0886c3
bryphe <bryphe@outrunlabs.com>
2019-09-18 09:44:24 -0700

    Add SetWindowIcon API

--- src/sdl2.re
+++ src/sdl2.re
@@ -1,15 +1,54 @@
+module Image = Image;
+module Size = {
+    type t = {
+        width: int,
+        height: int,
+    };
+};
+
+module Surface = {
+    type t;
+    external createFromImagePath: (string) => result(t, string) = "resdl_SDL_CreateRGBSurfaceFromImage";
+};
+
 module Window = {
     type t;
+    external create: (int, int, string) => t = "resdl_SDL_CreateWindow";
+    external getSize: t => Size.t = "resdl_SDL_GetWindowSize";
+    external setIcon: (t, Surface.t) => unit = "resdl_SDL_SetWindowIcon";
 }
 
 module OldGl = Gl;
 module Gl = {
     external setup: Window.t => unit = "resdl_SDL_GL_Setup";
     external swapWindow: Window.t => unit = "resdl_SDL_GL_SwapWindow";
+    external getDrawableSize: Window.t => Size.t = "resdl_SDL_GL_GetDrawableSize";
     include Gl;
 };
 
-external createWindow: unit => Window.t = "resdl_SDL_CreateWindow";
 external delay: int => unit = "resdl_SDL_Delay";
 external init: unit => int = "resdl_SDL_Init";
 
+module Event = {
+    type t =
+    | Quit
+    | MouseMotion;
+
+    let show = (v: t) => {
+        switch (v) {
+        | Quit => "Quit";
+        | MouseMotion => "MouseMotion";
+        }
+    };
+    
+    external poll: unit => option(t) = "resdl_SDL_pollEvent";
+};
+
+
+module Key = {
+    type t;
+};
+
+module MouseButton = {
+    type t;
+};
--- src/sdl2_wrapper.cpp
+++ src/sdl2_wrapper.cpp
@@ -18,6 +18,37 @@
 
 #include <SDL2/SDL.h>
 
+#define Val_none Val_int(0)
+static value
+Val_some( value v )
+{   
+    CAMLparam1( v );
+    CAMLlocal1( some );
+    some = caml_alloc(1, 0);
+    Store_field( some, 0, v );
+    CAMLreturn( some );
+}
+
+static value
+Val_ok( value v )
+{   
+    CAMLparam1( v );
+    CAMLlocal1( some );
+    some = caml_alloc(1, 0);
+    Store_field( some, 0, v );
+    CAMLreturn( some );
+}
+
+static value
+Val_error( value v )
+{   
+    CAMLparam1( v );
+    CAMLlocal1( some );
+    some = caml_alloc(1, 1);
+    Store_field( some, 0, v );
+    CAMLreturn( some );
+}
+
 extern "C" {
     CAMLprim value resdl_SDL_SetMainReady() {
         SDL_SetMainReady();
@@ -47,13 +78,137 @@ extern "C" {
         /* Turn on double buffering with a 24bit Z buffer.
          * You may need to change this to 16 or 32 for your system */
         SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-        SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+        //SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
         SDL_CreateRenderer(
                 win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
 
          gladLoadGLES2Loader((GLADloadproc) SDL_GL_GetProcAddress);
     }
+
+    CAMLprim value Val_SDL_Event(SDL_Event* event) {
+        CAMLparam0();
+        CAMLlocal2(ret, v);
+
+        if (event == NULL) {
+            ret = Val_none;
+        } else {
+            switch (event->type) {
+            case SDL_QUIT:
+               v = Val_int(0);
+               ret = Val_some(v);
+               break;
+            case SDL_MOUSEMOTION:
+                v = Val_int(1);
+                ret = Val_some(v);
+                break;
+            default:
+               ret = Val_none;
+            }
+        }
+
+        CAMLreturn(ret);
+    };
+
+    CAMLprim value resdl_SDL_pollEvent() {
+        CAMLparam0();
+        CAMLlocal1(ret);
+        SDL_Event e;
+
+        caml_release_runtime_system();
+        SDL_PollEvent(&e); 
+        caml_acquire_runtime_system();
+        ret = Val_SDL_Event(&e); 
+        CAMLreturn(ret);
+    }
+    
+    CAMLprim value resdl_SDL_GetWindowSize(value vWindow) {
+        CAMLparam1(vWindow);
+        CAMLlocal1(ret);
+        SDL_Window *win = (SDL_Window *)vWindow;
+        int width, height = 0;
+        SDL_GetWindowSize(win, &width, &height);
+        ret = caml_alloc(2, 0);
+        Store_field(ret, 0, Val_int(width));
+        Store_field(ret, 1, Val_int(height));
+        printf("SDL_GetWindowSize - width: %d height: %d\n", width, height);
+        CAMLreturn(ret);
+    }
+    
+    CAMLprim value resdl_SDL_GL_GetDrawableSize(value vWindow) {
+        CAMLparam1(vWindow);
+        CAMLlocal1(ret);
+        SDL_Window *win = (SDL_Window *)vWindow;
+        int width, height = 0;
+        SDL_GL_GetDrawableSize(win, &width, &height);
+        ret = caml_alloc(2, 0);
+        Store_field(ret, 0, Val_int(width));
+        Store_field(ret, 1, Val_int(height));
+        printf("SDL_GL_GetDrawableSize - width: %d height: %d\n", width, height);
+        CAMLreturn(ret);
+    }
+    
+    CAMLprim value resdl_SDL_SetWindowIcon(value vWindow, value vIcon) {
+        CAMLparam2(vWindow, vIcon);    
+
+        SDL_Window *win = (SDL_Window *)vWindow;
+        SDL_Surface *surface = (SDL_Surface *)vIcon;
+        SDL_SetWindowIcon(win, surface);
+
+        CAMLreturn(Val_unit);
+    };
+
+    CAMLprim value resdl_SDL_CreateRGBSurfaceFromImage(value vPath) {
+        CAMLparam1(vPath);
+        CAMLlocal1(ret);
+        // FROM: 
+        // https://wiki.libsdl.org/SDL_CreateRGBSurfaceFrom
+
+        int req_format = STBI_rgb_alpha;
+        int width, height, orig_format;
+        unsigned char* data = stbi_load(String_val(vPath), &width, &height, &orig_format, req_format);
+        if(data == NULL) {
+        
+          ret = Val_error(caml_copy_string(stbi_failure_reason()));
+        } else {
+
+        // Set up the pixel format color masks for RGB(A) byte arrays.
+        // Only STBI_rgb (3) and STBI_rgb_alpha (4) are supported here!
+        Uint32 rmask, gmask, bmask, amask;
+        #if SDL_BYTEORDER == SDL_BIG_ENDIAN
+          int shift = (req_format == STBI_rgb) ? 8 : 0;
+          rmask = 0xff000000 >> shift;
+          gmask = 0x00ff0000 >> shift;
+          bmask = 0x0000ff00 >> shift;
+          amask = 0x000000ff >> shift;
+        #else // little endian, like x86
+          rmask = 0x000000ff;
+          gmask = 0x0000ff00;
+          bmask = 0x00ff0000;
+          amask = (req_format == STBI_rgb) ? 0 : 0xff000000;
+        #endif
+
+        int depth, pitch;
+        if (req_format == STBI_rgb) {
+          depth = 24;
+          pitch = 3*width; // 3 bytes per pixel * pixels per row
+        } else { // STBI_rgb_alpha (RGBA)
+          depth = 32;
+          pitch = 4*width;
+        }
+
+        SDL_Surface* surf = SDL_CreateRGBSurfaceFrom((void*)data, width, height, depth, pitch,
+                                                     rmask, gmask, bmask, amask);
+        if (surf == NULL) {
+            ret = Val_error(caml_copy_string(SDL_GetError()));
+            stbi_image_free(data);
+        } else {
+            ret = Val_ok((value)surf);
+        }
+        }
+
+        CAMLreturn(ret);
+    };
 
     CAMLprim value resdl_test_poll() {
 
@@ -69,6 +224,7 @@ int hasStarted = 0;
                 switch (event.type) {
                 case SDL_QUIT:
                     /* Quit */
+                    SDL_Quit();
                     printf("QUIT!");
                     break;
                 case SDL_TEXTINPUT:
@@ -124,16 +280,77 @@ int hasStarted = 0;
         SDL_GL_SwapWindow(win);
     }
     
-    CAMLprim value resdl_SDL_CreateWindow() {
+    CAMLprim value resdl_SDL_CreateWindow(value vWidth, value vHeight, value vName) {
+        CAMLparam3(vWidth, vHeight, vName);
 
-      return (value)(SDL_CreateWindow("test", 
+        int width = Int_val(vWidth);
+        int height = Int_val(vHeight);
+        printf("name: %s\n, WidtH: %d\n height: %d\n", String_val(vName), width, height);
+
+          value vWindow = (value)(SDL_CreateWindow(String_val(vName), 
         SDL_WINDOWPOS_CENTERED, 
-        SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN));
+        SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI));
+
+        CAMLreturn(vWindow);
     }
+        
+        typedef enum PROCESS_DPI_AWARENESS {
+            PROCESS_DPI_UNAWARE = 0,
+            PROCESS_SYSTEM_DPI_AWARE = 1,
+            PROCESS_PER_MONITOR_DPI_AWARE = 2
+        } PROCESS_DPI_AWARENESS;
 
     CAMLprim value resdl_SDL_Init() {
         CAMLparam0();
         int ret = SDL_Init(SDL_INIT_VIDEO);
+
+        /*printf("SDL_INIT");
+        
+        void* userDLL;
+        BOOL(WINAPI *SetProcessDPIAware)(void); // Vista and later
+        void* shcoreDLL;
+        HRESULT(WINAPI *SetProcessDpiAwareness)(PROCESS_DPI_AWARENESS dpiAwareness); // Windows 8.1 and later
+        INT(WINAPI *GetScaleFactorForDevice)(int deviceType);
+        HRESULT(WINAPI *GetScaleFactorForMonitor)(HMONITOR hmon, int *pScale);
+
+        userDLL = SDL_LoadObject("USER32.DLL");
+        if (userDLL) {
+            printf("Found user DLL!\n");
+            SetProcessDPIAware = (BOOL(WINAPI *)(void)) SDL_LoadFunction(userDLL, "SetProcessDPIAware");
+        }
+
+        shcoreDLL = SDL_LoadObject("SHCORE.DLL");
+        if (shcoreDLL) {
+            printf("Found SHCOREd.ll!\n");
+            SetProcessDpiAwareness = (HRESULT(WINAPI *)(PROCESS_DPI_AWARENESS)) SDL_LoadFunction(shcoreDLL, "SetProcessDpiAwareness");
+            
+            GetScaleFactorForDevice = (INT(WINAPI *)(INT)) SDL_LoadFunction(shcoreDLL, "GetScaleFactorForDevice");
+            GetScaleFactorForMonitor = (HRESULT(WINAPI *)(HMONITOR, int*)) SDL_LoadFunction(shcoreDLL, "GetScaleFactorForMonitor");
+        }
+
+        if (GetScaleFactorForDevice) {
+            printf("Found getScaleFactorForDevice!\n");
+
+            int result = GetScaleFactorForDevice(0);
+            printf("--RESULT: %d\n", result);
+        }
+
+        if (GetScaleFactorForMonitor) {
+            printf("Found getScaleFactorForMonitor!!!\n");
+        }
+
+        if (SetProcessDpiAwareness) {
+            // Try Windows 8.1+ version
+            HRESULT result = SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE);
+            SDL_Log("called SetProcessDpiAwareness: %d", (result == S_OK) ? 1 : 0);
+        }
+        else if (SetProcessDPIAware) {
+            // Try Vista - Windows 8 version.
+            // This has a constant scale factor for all monitors.
+            BOOL success = SetProcessDPIAware();
+            SDL_Log("called SetProcessDPIAware: %d", (int)success);
+        }*/
+        
         CAMLreturn(Val_int(ret));
     }
   }

4c2ac30341f15f893d3b712fc1e6efe44a30350a
Sawyer Bergeron <sawyerbergeron@gmail.com>
2020-02-15 21:13:28 -0500

    Add getticks (#44)
    * Add SDL_GetTicks callable
    
    * Add getTicks external stub
    
    * Fix macro expansion
    
    * Fix formatting

--- src/sdl2_wrapper.cpp
+++ src/sdl2_wrapper.cpp
@@ -795,6 +795,14 @@ CAMLprim value resdl_SDL_WaitTimeoutEvent(value vTimeout) {
   CAMLreturn(ret);
 }
 
+CAMLprim value resdl_SDL_GetTicks() {
+    CAMLparam0();
+
+    int result = SDL_GetTicks();
+
+    CAMLreturn(Val_int(result));
+}
+
 CAMLprim value resdl_SDL_GetWindowSize(value vWindow) {
   CAMLparam1(vWindow);
   CAMLlocal1(ret);
